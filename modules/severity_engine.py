e89045754b5b35fab059e2b21f467a0071fe203c
Saranya0904 <145690457+Saranya0904@users.noreply.github.com>
2025-07-03 16:54:38 +0530

    Update severity_engine.py

--- modules/severity_engine.py
+++ modules/severity_engine.py
@@ -1,1 +1,25 @@
+def classify_severity(incident_type, artifact_count): 
 
+    incident_type = incident_type.lower() 
+
+ 
+
+    if incident_type == "malware infection" and artifact_count > 50: 
+
+        return "Critical" 
+
+    elif incident_type == "unauthorized access" and artifact_count > 20: 
+
+        return "High" 
+
+    elif incident_type == "data exfiltration" and artifact_count > 10: 
+
+        return "High" 
+
+    elif artifact_count > 5: 
+
+        return "Medium" 
+
+    else: 
+
+        return "Low" 
